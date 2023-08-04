bd6fa6606e51428d0b4a4743294e170cc8bc87c0
James Bellamy <james-ff@users.noreply.github.com>
2023-08-04 14:22:35 +0100

    Initial Commit

---
+++ BlinkinWeather/BlinkinWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkinWeatherApp.swift
+//  BlinkinWeather
+//
+//  Created by James Bellamy on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlinkinWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
